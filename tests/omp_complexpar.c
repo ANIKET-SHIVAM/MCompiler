76615bd93802026bcc1d16454833d0c44a50f1cb
Aniket Shivam <aniketsh@uci.edu>
2018-06-14 20:40:53 -0700

    Adding tests, specially for OMP tasks and OMP directives inside loop body. Adding file to keep track of changes in benchmarks to work with mCompiler.

--- tests/omp_complexpar.c
+++ tests/omp_complexpar.c
@@ -4,21 +4,21 @@
 #define N 100000
 
 int bucket[N];
-/* Comment this and mC works */
-#pragma omp threadprivate(bucket)
 int bucket2[N];
 
 int main(){
   int i=0, j=0;  
-  #pragma omp parallel private(i,j)  
+  #pragma omp parallel private(i,j)
   {
-    for(i=0;i<N;i++)
-      bucket[i] = i;
-
-    #pragma omp for
-    for(i=0;i<N;i++)
-      for(j=0;j<N;j++)
-        bucket2[bucket[i]] = i*j;
+  #pragma omp single nowait
+  {
+  for(i=0;i<N;i++){
+    bucket[i] = i;
+    #pragma omp task
+    for(j=0;j<N;j++)
+      bucket2[bucket[i]] = i*j;
+  }
+  }
   }
   long checksum = 0;
   for(i=0;i<N;i++)
---
+++ tests/static_extern.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+extern void external();
+int X=55;
+int glob=99;
+static int yo(int x, int y){
+	return x*y;
+}
+int foo(int);
+int bar(int*,int);
+
+int main(){
+	int i,j,k; long long sum1=0,sum2=0;
+	#pragma omp parallel
+  {
+    #pragma omp for 
+    for(i=0;i<100;i++){
+      printf("Threads: %d\n", omp_get_thread_num());
+      for(k=0;k<1000000;k++)
+        sum1+=i+k+pow(i,k);
+    }
+    printf("sum = %d\n", sum1);
+  }
+  if(sum1 = 0){
+	  for(j=0;j<10;j++){
+		  sum2 = yo(sum2,j);
+		  foo(sum2);
+	  }
+  }
+	bar(&sum1,sum2);
+	return 0;
+}
+
+int foo(int i){
+	return log(i);
+}
+int bar(int *i, int j){
+	return pow(*i,j);
+}
